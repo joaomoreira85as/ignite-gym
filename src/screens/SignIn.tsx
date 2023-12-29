c1182fda02373f13cdabc34555bccd54a3be12f6
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-29 15:10:44 -0300

    feat: add public routes

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,25 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  const handleNewAccount = () => {
+    navigation.navigate("signUp");
+  };
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -39,7 +47,11 @@ export function SignIn() {
           <Text color={"gray.100"} fontSize={"sm"} mb={3} fontFamily={"body"}>
             Ainda não tem acesso?
           </Text>
-          <Button title="Criar conta" variant={"outline"} />
+          <Button
+            title="Criar conta"
+            variant={"outline"}
+            onPress={handleNewAccount}
+          />
         </Center>
       </VStack>
     </ScrollView>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -3,15 +3,23 @@ import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 export function SignUp() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -40,7 +48,12 @@ export function SignUp() {
 
           <Button title="Criar e acessar" />
         </Center>
-        <Button mt={24} title="Voltar para o login" variant={"outline"} />
+        <Button
+          mt={24}
+          title="Voltar para o login"
+          variant={"outline"}
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
   );
