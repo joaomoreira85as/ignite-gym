d171c1ef448c69e96628bf07f2e6e8955eddd376
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-29 17:52:29 -0300

    feat: create home screen

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bgColor={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto
+        source={{ uri: "https://github.com/joaomoreira85as.png" }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color={"gray.100"} fontSize={"md"}>
+          Olá,
+        </Text>
+        <Heading color={"gray.100"} fontSize={"md"}>
+          João Moreira
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color={"gray.200"} size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
