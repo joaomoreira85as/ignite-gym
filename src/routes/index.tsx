86b89415f1d6062459a74d9a700de3e953436fbf
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-29 16:00:17 -0300

    feat: add app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { Box, useTheme } from "native-base";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
@@ -9,7 +10,7 @@ export function Routes() {
   return (
     <Box flex={1} bgColor={"gray.700"}>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
